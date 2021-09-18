4b10d64ab205a05a86e4aae9bc4c1f956342846b
Donald Kurangwa <donkur@my.yorku.ca>
2021-09-18 10:55:40 -0400

    backend auth and signup updates

--- the-hiring-place-backend/src/API/authentication.js
+++ the-hiring-place-backend/src/API/authentication.js
@@ -3,41 +3,93 @@ import * as jwtGenerator from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import db from "../../database/connection";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    // console.log(req.body)
-    const { email, password } = req.body;
-    db.query(
-      "SELECT * FROM <table name> WHERE email=?",
-      [email],
-      async (error, results) => {
-        // console.log(results);
-        if (results.length === 0) {
-          return res.status(401).json({
-            message: "Invalid Email or Password"
-          });
-        } else if (!(await bcrypt.compare(password, results[0].password))) {
-          return res.status(401).json({
-            message: "Invalid Email or Password"
-          });
-        } else {
-          const id = results[0].id;
+const getCredentials = (req) => {
+  return new Promise((resolve, reject) => {
+    // Validate input
+    const { error } = validateAuthForm(req.body);
+    if (error) return reject(error.details[0].message);
 
-          const token = jwtGenerator.sign(
-            { id },
-            process.env.ACCESS_JWT_SECRET,
-            {
-              expiresIn: process.env.JWT_EXPIRES_IN,
-            }
-          );
-          return res.status(200).json({ token });
-        }
-      }
+    let { login_id, password } = req.body;
+
+    const payload = {
+      login_id,
+      password,
+    };
+
+    return resolve(payload);
+  });
+};
+
+const validateUser = (userCredentials, usersFullCredentials) => {
+  return new Promise(async (resolve, reject) => {
+    const index = usersFullCredentials.findIndex(
+      (userFullCredentials) =>
+        userFullCredentials.login_id === userCredentials.login_id
     );
+
+    if (index === -1) {
+      return reject({ error: "Invalid ID or password.", status: 400 });
+    } else {
+      try {
+        const validPassword = await bcrypt.compare(
+          userCredentials.password,
+          usersFullCredentials[index].password
+        );
+
+        if (!validPassword)
+          return reject({ error: "Invalid ID or password.", status: 400 });
+        return resolve(usersFullCredentials[index]);
+      } catch (error) {
+        return reject(error);
+      }
+    }
+  });
+};
+
+const getToken = (validUser) => {
+  const payload = {
+    login_id: validUser.login_id,
+    isadmin: validUser.isadmin,
+  };
+  return jwtGenerator.sign(payload, `${process.env.ACCESS_JWT_SECRET}`);
+};
+
+router.post("/", async (req, res) => {
+  const sql = `SELECT e.email, e.password, a.isadmin 
+    FROM emrconn.employee e 
+    INNER JOIN emrconn.admin a 
+    ON ( e.employee_id = a.employee_id 
+      AND e.employee_id = a.employee_id 
+      AND e.employee_id = a.employee_id )`;
+
+  try {
+    const userCredentials = await getCredentials(req);
+    const usersFullCredentials = await db.query(sql);
+    const validUser = await validateUser(userCredentials, usersFullCredentials);
+    const token = getToken(validUser);
+    return res.status(201).json({ token: token });
   } catch (error) {
-    console.log(error);
+    if (error.status == 400) {
+      return res.status(400).json({ message: "Invalid email or password!" });
+    } else {
+      return res.status(500).send(error);
+    }
+  } finally {
   }
 });
+
+const validateAuthForm = (reqBody) => {
+  const schemaUserForm = Joi.object({
+    login_id: Joi.string().min(5).required(),
+    password: Joi.string().min(8).required(),
+  });
+
+  return schemaUserForm.validate(reqBody);
+};
+
 export default router;
