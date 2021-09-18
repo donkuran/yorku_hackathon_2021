2821c5bff1253727b90f2a20d9a4bed7ca493598
nmuceros <nolita.muceros@gmail.com>
2021-09-18 11:37:41 -0400

    moves code to gradProfilePage and adds gradWorkPreference page

--- the-hiring-place/src/components/pages/gradProfilePage.js
+++ the-hiring-place/src/components/pages/gradProfilePage.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col, Button, Container } from "reactstrap";
+import '../../css/gradProfilePage.css';
+import profilePhoto from '../../images/profile2.png';
+import tooltip from '../../images/tooltip.png';
+import settings from '../../images/settings.svg';
+import profile from '../../images/profile.svg';
+import alert from '../../images/alert.svg';
 
 
 const GradProfilePage = () => {
@@ -7,7 +13,127 @@ const GradProfilePage = () => {
 
   return (
     <Container>
-   
+      <div className="gradProfile-wrapper">
+          <div className="header">
+              <span className="header-yoh">YOH</span>
+              <input id="logout" type="button" style={{float:"right"}} onclick="logout()" value="Logout" />
+              <img id="settings" src={ settings } alt="settings icon" style={{float:"right"}} onclick="settings()" />
+              <img id="profile" src={ profile } alt="profile icon" style={{float:"right"}} onclick="profile()" />
+              <img id="alert" src={ alert } alt="alert icon" style={{float:"right"}} onclick="alert()" />
+          </div>
+
+        <form>
+          <div className="topnav">
+            <a id="profile" href="#">Profile</a>
+            <a id="workPreference" href="#">Work Preference</a>
+            <input id="view" type="button" style={{float:"right"}} onclick="viewGradProfile()" value="View" />
+            <input id="edit" type="button" style={{float:"right"}} onclick="editGradProfile()" value="Edit" />
+            <input id="save" type="button" style={{float:"right"}} onclick="saveGradProfile()" value="Save" />
+          </div>
+
+          <div className="row">
+              <div className="leftcolumn">
+                  <div className="card">
+                        <div className="profilePicture-container">
+                            <img className="profilePhoto" src={ profilePhoto } alt="profilePhoto"/>   
+                        </div>
+                        <input id="upload" type="button" onclick="upload()" value="Upload" />
+                        <div className="profile-detail-container">
+                            <label className="label">Name</label> <br />
+                            <input id="name" className="input-text" type="text" placeholder="Name" />  <br />
+
+                            <label className="label">Pronouns</label>
+                            <label className="roleTitle-label">Role/Title</label> <br />
+                            
+                            <input id="pronouns" className="input-text" type="text" placeholder="Pronouns" />
+                            <input id="roleTitle" 
+                                  className="input-text" 
+                                  style={{margin: "0px 14px"}} 
+                                  ype="text" placeholder="Role/Title" 
+                            />
+                            <label className="label">Program</label> <br />
+                            <input id="program" className="input-text" type="text" placeholder="Program" />  <br />
+                            
+                            <label className="label">Graduation Year (Or Expected)</label>
+                            <input id="graduationYear" className="input-text" type="text" placeholder="YYYY" /> <br />
+
+                            <label className="label">Location</label> <br />
+                            <input id="location" className="input-text" type="text" placeholder="City, Province" />
+                                  
+                            <select id="country" name="country" style={{margin: "0px 10px"}}>        
+                              <option value="canada">Canada</option>
+                              <option value="usa">USA</option>
+                            </select>
+
+                        </div>
+                  </div>
+                  <div className="card">
+                      <div className="cardTitle">Links</div>
+                      <input id="linkedIn" type="text" placeholder="LinkedIn" />
+                      <input id="websitePortfolio" type="text" placeholder="Website Or Portfolio" />
+                      <input id="twitter" type="text" placeholder="Twitter" />
+                      <input id="instagram" type="text" placeholder="Instagram" />
+                      <input id="resume" type="text" placeholder="Resume Link" />
+                  </div>
+            </div>
+
+            <div className="rightcolumn">
+                  <div className="card">
+                    <div className="cardTitle-aboutMe">About Me</div>
+
+                  {/*  aboutMe-tooltip-content="Tooltip: Remember this is an opportunity to tell recruiters about why you
+                                    would be a great addition to a team/company" */}
+                    <div className="aboutMe-tooltip-container" >
+                          <img className="aboutMe-tooltip" src={ tooltip } alt="tooltip icon"/>   
+                    </div>   
+
+                    <div className="aboutMeDesc-container">
+                        <textarea id="aboutMeDescription" type="text" placeholder="Description" required />
+                    </div>
+                  </div>
+                  <div className="card">
+                      <div className="cardTitle">Education</div>
+                      <input id="programCourse" type="text" placeholder="Program / Course" required />
+                      <label className="startDate-label">Start Date</label>
+                      <label className="endDate-label">End Date</label>
+                      <input id="startDate" className="input-text" type="text" placeholder="MM / YYYY" />
+                      <input id="endDate" className="input-text" type="text" placeholder="MM / YYYY" />
+                  </div>
+
+                  <div className="card">
+                    <div className="techskills-container">
+                        <div className="cardTitle">Technical Skills</div>
+                        <div className="techSkills">This is where the added skills will be displayed</div>
+                        <input id="enterTechSkills" type="text" placeholder="Enter Skills" required />
+                        <input id="addTechSkills" type="button" onclick="addTechSkills()" value="Add" />
+                    </div>
+                    <div className="personalSkills-container">
+                        <div className="cardTitle">Personal Skills</div>
+                        <div className="personalSkills">This is where the added skills will be displayed</div>
+                        <input id="enterPersonalSkills" type="text" placeholder="Enter Skills" required />
+                        <input id="addPersonalTechSkills" type="button" onclick="addPersonalSkills()" value="Add" />
+                    </div>
+
+                    <div className="languages">
+                        <div className="cardTitle">Languages</div>
+                        <input id="languages" type="text" placeholder="Languages" required />
+                    </div>                    
+
+
+                  </div>
+              </div>
+          </div>
+        </form>
+
+        <div class="footer">
+            <span className="yoh1">YOH</span> <br />
+            <span className="yoh2">York Opportunity Hub</span> <br />
+            <input id="contact" type="button" onclick="Contact()" value="Contact" /> <br />
+            <span className="privacy-policy">Privacy Policy</span> <br />
+            <span className="terms-and-conditions">Terms and Conditions</span> <br />
+            <span className="copyright">Yok University | School of Continuing Studies. All Rights Reserved. 2021</span>
+        </div>
+      </div>  
 
     </Container>
   );
---
+++ the-hiring-place/src/components/pages/gradWorkPreference.js
@@ -0,0 +1,200 @@
+import React, { useEffect, useState } from "react";
+import { Row, Col, Button, Container, FormGroup } from "reactstrap";
+import '../../css/gradWorkPreference.css';
+import profilePhoto from '../../images/profile2.png';
+import tooltip from '../../images/tooltip.png';
+import settings from '../../images/settings.svg';
+import profile from '../../images/profile.svg';
+import alert from '../../images/alert.svg';
+
+
+const GradWorkPreference = () => {
+
+  const images = [profilePhoto, tooltip];
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  
+  return (
+
+    <Container>
+      <div className="gradProfile-wrapper">
+        <form>
+          <div className="row">
+              <div className="leftcolumn">
+
+                  <div className="card">
+                      <div className="jobPreference">Describe what you are looking for in your next role</div>
+
+                       {/*  aboutMe-tooltip-content="Tooltip: Remember this is an opportunity to tell recruiters about why you
+                                      would be a great addition to a team/company" */}
+                        <div className="jobPreference-tooltip-container" >
+                            <img className="jobPreference-tooltip" src={ tooltip } alt="tooltip icon"/>   
+                       </div>   
+                       <div className="jobPreference-desc-container">
+                           <textarea id="jobPreference-description" type="text" placeholder="Description" required />
+                         </div>
+                  </div>
+
+                  <div className="card">
+                      <div className="interests-container">
+
+                          <div className="cardTitle">Interests</div>
+                          <div className="drop-down">                     
+                              <select id="accomplishments">
+                                  <option value="" disabled selected hidden>A professional accomplishment I'm proud of is...</option>
+                                  <option value="1">Option1</option>
+                                  <option value="2">Option2</option>
+                                  <option value="3">Option3</option>
+                              </select> 
+                              <input id="accomplishments" className="textbox" type="text" placeholder="Your Answer" />   
+                          </div>
+                          <div className="drop-down">  
+                              <select id="superpower">
+                                  <option value="" disabled selected hidden>My professional superpower is...</option>
+                                  <option value="1">Option1</option>
+                                  <option value="2">Option2</option>
+                                  <option value="3">Option3</option>
+                              </select> 
+                              <input id="superpower" className="textbox" type="text" placeholder="Your Answer" />                        
+                          </div>
+                          <div className="drop-down">  
+                              <select id="iLikeTo">
+                                  <option value="" disabled selected hidden>When not working I like to...</option>
+                                  <option value="1">Option1</option>
+                                  <option value="2">Option2</option>
+                                  <option value="3">Option3</option>
+                              </select> 
+                              <input id="iLikeTo" className="textbox" type="text" placeholder="Your Answer" />                        
+                          </div>
+                      </div>                 
+                  </div>
+
+                  <div className="card">
+                      <div className="cardTitle">How important is it that your next role has a flexible remote work policy?</div>
+                      <label class="radio-button-container">
+                         <input type="radio" checked="checked"/>
+                         <span className="checkmark">Not Important</span>
+                      </label>
+                      <label className="radio-button-container">
+                         <input type="radio"/>
+                         <span className="checkmark">Very Important</span>
+                      </label>
+                  </div>
+
+                  <div className="card">
+                     <div className="industry-container">
+                        <div className="cardTitle">Which markets are you most interested in working in</div>
+                        <div className="industry">This is where the added Industry will be displayed</div>
+                        <input id="enterIndustry" type="text" placeholder="Industry" required />
+                        <input id="addIndustry" type="button" onclick="addIndustry()" value="Add" />
+                     </div>
+                  </div>
+            </div>
+
+            <div className="rightcolumn">
+
+                  <div className="card">
+                        <div className="cardTitle">What type of positions are you interested in?</div>
+                        <label class="checkmark-container">
+                          <input type="checkbox" />
+                          <span className="checkmark">internship</span>
+                        </label>
+                        <label className="checkmark-container">
+                          <input type="checkbox"/>
+                          <span className="checkmark">Contract</span>
+                        </label>
+                        <label className="checkmark-container">
+                          <input type="checkbox"/>
+                          <span className="checkmark">Full time</span>
+                        </label>
+                        <label className="checkmark-container">
+                          <input type="checkbox"/>
+                          <span className="checkmark">Part time</span>
+                        </label>                                                
+                    </div>
+
+                    <div className="card">
+                        <div className="cardTitle">What size of company would you like to work in?</div>
+                        <label class="checkmark-container">
+                          <input type="checkbox"/>
+                          <span className="checkmark">Small (1-10 employees)</span>
+                        </label>
+                        <label className="checkmark-container">
+                          <input type="checkbox"/>
+                          <span className="checkmark">CMid-size (50-200 employees)</span>
+                        </label>
+                        <label className="checkmark-container">
+                          <input type="checkbox"/>
+                          <span className="checkmark">Large (201-500 employees)</span>
+                        </label>
+                        <label className="checkmark-container">
+                          <input type="checkbox"/>
+                          <span className="checkmark">Very Large (501-1000+ employees)</span>
+                        </label>                                                
+                    </div>
+
+                    <div className="card">
+                      <div className="cardTitle">What is your desired annual salary?</div>
+                      <input id="desiredSalary" className="textbox" type="text" placeholder="$" />   
+                  </div>
+
+                  <div className="card">
+                     <div className="workLocation-container">
+                        <div className="cardTitle">Which locations do you want to work in?</div>
+                        <div className="workLocation">This is where the added City will be displayed</div>
+                        <input id="enterWorkLocation" type="text" placeholder="City" required />
+                        <input id="addWorkLocation" type="button" onclick="addWorkLocation()" value="Add" />
+                        <label class="checkmark-container">
+                          <input type="checkbox" />
+                          <span className="checkmark">Open to working remotely</span>
+                        </label>
+                     </div>
+                  </div>   
+
+                  <div className="card">
+                      <div className="cardTitle">What is important to you in your next role?</div>
+                      <label class="checkmark-container">
+                        <input type="checkbox" />
+                        <span className="checkmark">Mentorshiip</span>
+                      </label>
+                      <label className="checkmark-container">
+                        <input type="checkbox"/>
+                        <span className="checkmark">Growth within the company</span>
+                      </label>
+                      <label className="checkmark-container">
+                        <input type="checkbox"/>
+                        <span className="checkmark">Challenging problems to solve</span>
+                      </label>
+                      <label className="checkmark-container">
+                        <input type="checkbox"/>
+                        <span className="checkmark">Having a voice in the company or team direction</span>
+                      </label>  
+                      <label className="checkmark-container">
+                        <input type="checkbox"/>
+                        <span className="checkmark">Opportunities to learn and develop new skills</span>
+                      </label>  
+                      <label className="checkmark-container">
+                        <input type="checkbox"/>
+                        <span className="checkmark">A company with a strong growth trajectory</span>
+                      </label>                                                                                                
+                  </div>
+              </div>
+              <section>
+                  <input id="submit" type="button" onclick="submit()" value="Submit" />
+                  <p>That's it!</p>
+                  <p>Click Submit and your profile is ready to be seen by recruiters.</p>
+                  <p>Use this link to add your YOH! Profile URL to share.</p>
+              </section>                            
+          </div>
+
+        </form>
+      </div>  
+
+</Container>
+
+  );
+};
+
+export default GradWorkPreference;
+
+
